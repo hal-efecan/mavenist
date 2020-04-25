82fae08f2716ec43963ec71e9916970ae7e5f632
halil <hefecan@gmail.com>
2020-04-25 09:42:58 +0100

    added sticky nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Nav from './nav'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 import headerStyles from '../styles/header.module.scss'
@@ -9,8 +9,32 @@ const Header = () => {
     const data = useStaticQuery(query)
     const { title } = data.site.siteMetadata
 
+
+    let window;
+    if (typeof window !== 'undefined') {
+        // return global.window = {}
+        let prevScrollPos = window.pageYOffset;
+        return prevScrollPos
+    }
+    
+    useEffect(() => {
+
+        window.onscroll = () => {
+            let currScrollPos = window.pageYOffset
+            
+            if(prevScrollPos > currScrollPos) {
+                let myDiv = document.querySelector('#head')
+                console.log(myDiv)
+                myDiv.style.top = "0"
+            } else {
+                document.querySelector('#head').style.top = "-140px"
+            }
+            prevScrollPos = currScrollPos
+        }
+    },[])
+
     return (
-        <div className={headerStyles.header}>
+        <div id="head" className={headerStyles.header}>
             
                 <h1 className={headerStyles.title}>
                     <Link className={headerStyles.link} to={`/`}>
