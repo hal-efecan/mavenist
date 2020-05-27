b69c5d8ce84e28738bd4fc00c3c612a1826c8934
halil <hefecan@gmail.com>
2020-05-27 15:27:45 +0100

    updated meta tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -164,6 +164,18 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-html-comments`,
+      options: {
+        files: ['./public/**/*.html', './public/*.html'],
+        comment: [
+          {
+            regexp: /<custom-tag>(.*?)<\/custom-tag>/g,
+            comment: `<!-- Digital window verification 001 -->`,
+            },
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
