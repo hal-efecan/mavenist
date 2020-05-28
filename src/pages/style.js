661f72ff5242aa015a6b37c84c83b6c3ba51826a
halil <hefecan@gmail.com>
2020-05-28 21:30:42 +0100

    seo component fix

--- src/pages/style.js
+++ src/pages/style.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import Layout from '../components/layout'
 import StyleArticles from '../components/style'
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
+
+import SEO from 'react-seo-component'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
 import main from '../styles/main.module.scss'
 
-const Style = () => {
+const Style = ({ data }) => {
+    const {
+        description,
+        title,
+        image,
+        siteUrl,
+        siteLanguage,
+        siteLocale,
+        social: {
+            twitter
+        }
+      } =  useSiteMetadata()
+
     return (
         <Layout>
-            <SEO title="Style" description="All style related content"/>
+            {/* <SEO title="Style" description="All style related content"/> */}
+            <SEO
+                title={title}
+                // titleTemplate={siteName}
+                description={description || 'nothin’'}
+                image={`${siteUrl}${image}`}
+                pathname={siteUrl}
+                siteLanguage={siteLanguage}
+                siteLocale={siteLocale}
+                twitterUsername={twitter}
+                />
             <div className={main.container}> 
                 <StyleArticles />  
             </div>
--- src/pages/thoughts.js
+++ src/pages/thoughts.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import Layout from '../components/layout'
 import ThoughtsArticles from '../components/thoughts'
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
+
+import SEO from 'react-seo-component'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
 import main from '../styles/main.module.scss'
 
-const Thoughts = () => {
+const Thoughts = ({ data }) => {
+    const {
+        description,
+        title,
+        image,
+        siteUrl,
+        siteLanguage,
+        siteLocale,
+        social: {
+            twitter
+        }
+      } =  useSiteMetadata()
+
     return (
         <Layout>
-            <SEO title="Thoughts" description="All Thoughts related content"/>
+            {/* <SEO title="Thoughts" description="All Thoughts related content"/> */}
+            <SEO
+                title={title}
+                // titleTemplate={siteName}
+                description={description || 'nothin’'}
+                image={`${siteUrl}${image}`}
+                pathname={siteUrl}
+                siteLanguage={siteLanguage}
+                siteLocale={siteLocale}
+                twitterUsername={twitter}
+                />
             <div className={main.container}>
                 <ThoughtsArticles />
             </div>
