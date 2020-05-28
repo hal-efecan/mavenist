7ab5bdf0f0570663a1c175b61b2b13e59f4e6770
halil <hefecan@gmail.com>
2020-05-28 21:24:48 +0100

    seo component fix

--- src/pages/fashion.js
+++ src/pages/fashion.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import Layout from '../components/layout'
 import FashionArticles from '../components/fashion'
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
+import SEO from 'react-seo-component'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
 import main from '../styles/main.module.scss'
 
-const Fashion = () => {
+const Fashion = ({ data }) => {
+
+    const {
+        description,
+        title,
+        image,
+        siteUrl,
+        siteLanguage,
+        siteLocale,
+        social: {
+            twitter
+        }
+      } =  useSiteMetadata()
+
     return (
         <Layout>
-            <SEO title="Fashion" description="All Fashion related content" />
+            {/* <SEO title="Fashion" description="All Fashion related content" /> */}
+            <SEO
+                title={title}
+                // titleTemplate={siteName}
+                description={description || 'nothin’'}
+                image={`${siteUrl}${image}`}
+                pathname={siteUrl}
+                siteLanguage={siteLanguage}
+                siteLocale={siteLocale}
+                twitterUsername={twitter}
+                />
             <div className={main.container}>
                 <FashionArticles />
             </div>
