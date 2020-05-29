dba655f7393e5927773e6e9643eeed9d9aec782f
halil <hefecan@gmail.com>
2020-05-29 11:36:12 +0100

    rss image fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,15 +50,10 @@ module.exports = {
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                //   enclosure: image && {
-                //     url: site.siteMetadata.siteUrl + image.publicURL,
-                // },
-                  custom_elements: [{ "content:encoded": edge.node.html }, {
-                    'article': {
-                      _attr: {
-                        href: site.siteMetadata.siteUrl + image.publicURL
-                      }
-                  }}], // "content:encoded"
+                  enclosure: image && {
+                    url: site.siteMetadata.siteUrl + image.publicURL,
+                },
+                  custom_elements: [{ "content:encoded": edge.node.html }, {image_link: site.siteMetadata.siteUrl + image.publicURL }] // "content:encoded"
                 })
               })
             },
