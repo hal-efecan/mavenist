5e8b973b544179e40654e8f509d18571873b2ab4
halil <hefecan@gmail.com>
2020-06-19 17:25:57 +0100

    new post update

--- src/components/banners/shopBanner.js
+++ src/components/banners/shopBanner.js
@@ -5,19 +5,17 @@ import { Base, Blurb } from '../../styled/banner/shopBanner'
 
 const ShopBanner = () => {
     return (
-                <div style={{
-                    width: `max-content`, margin: `0 auto`, backgroundColor:`coral`
-                }}>
+                <div style={{ width: `max-content`, margin: `0 auto` }}>
 
-                <Link 
-                style={{ boxShadow: `none`, width: `100%` }} 
-                to={`/shop`}>
+                    <Link 
+                    style={{ boxShadow: `none`, width: `100%` }} 
+                    to={`/shop`}>
 
-                <Base>
-                    <Blurb>Shop the edit</Blurb>
-                </Base>
-                
-                </Link>
+                    <Base>
+                        <Blurb>Shop the edit</Blurb>
+                    </Base>
+                    
+                    </Link>
 
                 </div>
     )
