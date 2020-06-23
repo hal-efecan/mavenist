6ab75397a302c6237b22e3d83036e30a094bf3cd
halil <hefecan@gmail.com>
2020-06-23 16:28:37 +0100

    updated viewport width

--- src/components/custom/iterations-of-nike-air-max/airmax90.js
+++ src/components/custom/iterations-of-nike-air-max/airmax90.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
 
 const Airmax90 = () => {
 
+    const [ width, setWidth ] = useState(null)
+
     const data = useStaticQuery(query)
     const fluid = data.imageSharp.fluid
 
+    useEffect(() => {
+        if(window.screen.width === 375) {
+            setWidth(true)
+        } else {
+            setWidth(false)
+        }
+    })
+
     return (
-        <div style={{ 
-            margin: `40px auto 0 auto`,
-            width: `600px`
-            }}>
-            <Img fluid={fluid} />            
+        <div>
+            {
+                width ? 
+                    <div     
+                    style={{ 
+                        margin: `0 auto` 
+                        }}>
+                    
+                        <Img fluid={fluid} />
+                    </div>
+                :
+
+                <div     
+                style={{ 
+                    width: `600px`, 
+                    margin: `0 auto` 
+                    }}>
+                
+                    <Img fluid={fluid} />
+                </div>
+            }
         </div>
     )
 }
